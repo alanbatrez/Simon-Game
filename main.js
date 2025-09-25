5df71bf1040746ebb81f32af91a8ca73173a6402
alanbatrez <alanbatrez@hotmail.com>
2025-09-24 22:07:07 -0700

    sequence ready, need to fix animations, proper restart

--- main.js
+++ main.js
@@ -1,30 +1,46 @@
-
 const btnAll = document.querySelectorAll('.btn');
 const backGround = document.querySelector('body');
 const buttonColors = ['red', 'blue','green', 'yellow'];
 const title  = document.querySelector('h1');
+
 let gamePattern = [];
 let userPattern = [];
-
-
+let isStartkeyPresses = false;
+let round = 1;
 
 /*
 Function created to re-assign random value to variable randomNumber range 0 - 3
- */
-function nextSequence(){
+*/
+function nextSequence() {
+    userPattern = [];  // clean previues sequence
+
     let randomNumber = Math.floor(Math.random() * 4);
-    let randomChosenColour = randomNumber;
-    gamePattern.push(randomChosenColour);
+    let randomColor = buttonColors[randomNumber];
+    gamePattern.push(randomColor);
+
+    // Reproduce toda la secuencia acumulada
+    let i = 0;
+    let interval = setInterval(() => {
+        let color = gamePattern[i];
+        flash(color);
+        playSound(color);
+        i++;
+        if (i >= gamePattern.length) {
+            clearInterval(interval);
+        }
+    }, 800);
 }
 
-
-
-function selectedElement(randomChosenColour){
-    return document.getElementById(buttonColors[randomChosenColour]);
+function flash(color) {
+    const el = document.getElementById(color);
+    el.classList.add("pressed");
+    setTimeout(() => el.classList.remove("pressed"), 300);
 }
 
-let isStartkeyPresses = false;
-
+function playSound(color) {
+    let sound = new Audio(`sounds/${color}.mp3`);
+    sound.play();
+}
 
 document.addEventListener('keydown', function(e){
     if(e.key.toLowerCase() === 'a') {
@@ -33,80 +49,63 @@ document.addEventListener('keydown', function(e){
             isStartkeyPresses = true;
 
             nextSequence();
-
-            playStartGameSound();
-
             changeTitle();
-
-
-
-
-            let correctChoice = true;
-
-            // while(correctChoice){
-            //     let score = 0;
-            // }
-
-            pressedAnimation();
-            console.log(selectedElement(gamePattern[0]).attributes[1].nodeValue);
-            btnAll.forEach(button => button.addEventListener("click",function(){
-                let userClickChoice= button.getAttribute("id");
-                if(userClickChoice === selectedElement(gamePattern[0]).getAttribute('id')){
-                    console.log("Correct answer clicked");
-                    correctPoint();
-                }
-                else{
-                    wrongMove();
-
-                }
-            }));
+            pressedButton();
         }
-
-    }
-    else{
-
+    } else {
         title.classList.add("game-over");
-        let timeout = setTimeout(function (){
-            title.classList.remove("game-over");
-        },200)
-        console.log(isStartkeyPresses);
+        setTimeout(() => title.classList.remove("game-over"), 200);
     }
-
 });
 
-
-
-function playStartGameSound() {
-    let sound = new Audio(`sounds/${selectedElement(gamePattern[0]).getAttribute('id')}.mp3`);
-    sound.play();
-}
-
 function wrongMove(){
-    let wrongSound = new Audio('/sounds/wrong.mp3');
+    let wrongSound = new Audio('sounds/wrong.mp3');
     wrongSound.play();
     backGround.classList.add("game-over");
-    setTimeout(function (){
-        backGround.classList.remove("game-over");
-    }, 200);
+    setTimeout(() => backGround.classList.remove("game-over"), 200);
 }
 
-const correctPoint = ()=>{
-    backGround.classList.add("green");
-    setTimeout(function (){
-        backGround.classList.remove("green");
-    },100)
-}
-
-
-
-const pressedAnimation = ()=>{
-    let item = selectedElement(gamePattern[0]);
+function pressedAnimation(color){
+    let item = document.getElementById(color);
     item.classList.add("pressed");
-    setTimeout(function (){
-        item.classList.remove("pressed");
-    },400)
-}
-function changeTitle(){
-    title.innerText = "Round 1";
+    setTimeout(() => item.classList.remove("pressed"), 400);
 }
 
+function changeTitle(){
+    title.innerText = `Round ${round}`;
+    round++;
+}
+
+function pressedButton() {
+    btnAll.forEach(button => {
+        button.addEventListener("click", function () {
+            let userClickChoice = button.getAttribute("id");
+            userPattern.push(userClickChoice);
+
+            console.log(userPattern);
+            console.log("You clicked:", userClickChoice);
+
+            let currentIndex = userPattern.length - 1;
+
+            if (gamePattern[currentIndex] === userPattern[currentIndex]) {
+                console.log("Correct!" + correctPoint());
+
+                if (userPattern.length === gamePattern.length) {
+                    console.log("Round completo, siguiente secuencia");
+                    setTimeout(() => {
+                        nextSequence();
+                        changeTitle();
+                    }, 1000);
+                }
+            } else {
+                console.log("Error!");
+                wrongMove();
+            }
+        });
+    });
+}
+
+const correctPoint = () => {
+    backGround.classList.add("green");
+    setTimeout(() => backGround.classList.remove("green"), 100);
+}
